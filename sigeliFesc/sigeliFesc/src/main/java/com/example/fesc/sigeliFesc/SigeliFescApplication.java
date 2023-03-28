e9f32841d635da50ffe80ad4e36bcab7d0b9ecdd
Eduar0425y <xavieravendano9@gmail.com>
2023-03-28 06:40:41 -0500

    creación del proyecto inical

---
+++ sigeliFesc/sigeliFesc/src/main/java/com/example/fesc/sigeliFesc/SigeliFescApplication.java
@@ -0,0 +1,13 @@
+package com.example.fesc.sigeliFesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigeliFescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigeliFescApplication.class, args);
+	}
+
+}
---
+++ sigeliFesc/sigeliFesc/src/test/java/com/example/fesc/sigeliFesc/SigeliFescApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fesc.sigeliFesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigeliFescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
